72408482895d0c8e12129704078b618287d7d1d4
huaxiaofeng <huaxiaofeng@wanshifu.com>
2016-03-15 09:39:44 +0800

    配置文件

---
+++ config/define.php
@@ -0,0 +1,7 @@
+<?php
+  
+
+#ini_set("session.save_handler", "memcache");
+#ini_set("session.save_path", "tcp://172.16.3.233:11211");
+#ini_set("session.save_path", "tcp://192.168.1.147:11211");
+ 
